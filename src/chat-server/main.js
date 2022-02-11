1b98130ecd40b172255d1179ea30cbd673377130
Mattia <mattia.ceschin@gmail.com>
2022-02-11 15:18:17 +0100

    add feature rooms

--- src/chat-server/main.js
+++ src/chat-server/main.js
@@ -18,9 +18,23 @@ require('dotenv').config();
     console.log("Path: "+wss.options.path);
     const clients = new Map();
 
-    //id_chats
-    const chatClients = []
-
+    //rooms
+    const rooms = []
+    /*
+    {
+        id_room:'balbalblabla',
+        clients:[
+            {
+                id:'81298dj19jd1',
+                nickname:'pippo'
+            },
+            {
+                ...
+            }
+        ]
+    }
+    */
+    const MAX_NUMBER_CLIETNS_ROOM = process.env.MAX_NUM
     wss.on('connection', (ws) =>{
         const id = uuid.v4();
         clients.set(ws, id)
@@ -34,14 +48,47 @@ require('dotenv').config();
                     id: clients.get(ws),
                     nickname: dataJson.nickname
                 }
-                chatClients.push(client)
+                
+                //room
+                var insert_done = false
+                var id_room_client = null;
+                do
+                {
+
+                    //try insert
+                    for(var i=0; i<rooms.length; i++)
+                    {
+                        if(rooms[i].clients.length < MAX_NUMBER_CLIETNS_ROOM)
+                        {
+                            id_room_client = rooms[i].id_room
+                            rooms[i].clients.push(client)
+                            insert_done = true
+                            break
+                        }
+                    }
+
+                    //check
+                    if(rooms.length <= 0 || insert_done == false)
+                    {
+                        id_room_client = uuid.v4()
+                        rooms.push({
+                            id_room:id_room_client,
+                            clients:[]
+                        })
+                    }
+                }while(insert_done == false)
+
+                //send confirm registration
                 ws.send(JSON.stringify({action: 'registration', id:clients.get(ws), nickname: dataJson.nickname}))
                 
-                //sent other contacts
-                chatClients.forEach(item => {
+                //sent other contacts for room
+                const room = rooms.find(element => element.id_room === id_room_client)
+                room.clients.forEach(item =>{
                     const ws = [...clients].find(([key, val]) => val == item.id)[0]
-                    ws.send(JSON.stringify({action: 'newContact', chatClients}))
-                });
+                    const find = room.clients.find(element => element.id === item.id) || null
+                    if(find != null)
+                        ws.send(JSON.stringify({action: 'UpdateRoom', room}))
+                })
             }else if(dataJson.action === 'send'){
                 console.log('un messagio da inoltrare')
                 console.log(dataJson)
@@ -53,23 +100,44 @@ require('dotenv').config();
         })
 
         ws.on("close", () => {
-            var pos = 0
-
-            chatClients.forEach(item => {
-                if(item.id === clients.get(ws))
-                {
-                    chatClients.splice(pos, 1); 
-                }
-                ++pos
-            })
             var id = clients.get(ws)
+            //find id room
+            var id_room_client = null
+            for(var i=0; i<rooms.length; i++)
+            {
+                const find = rooms[i].clients.find(element => element.id === id) || null
+                    if(find != null)
+                    {
+                        id_room_client=rooms[i].id_room
+                        break
+                    }
+            }
 
-            chatClients.forEach(item => {
+            //get class room
+            const room = rooms.find(element => element.id_room === id_room_client)
+            room.clients.forEach(item =>{
                 const ws = [...clients].find(([key, val]) => val == item.id)[0]
-                ws.send(JSON.stringify({action: 'deleteContact', id}))
-            });
+                const find = room.clients.find(element => element.id !== id) || null
 
+                //send clients that one client is closed
+                if(find != null)
+                    ws.send(JSON.stringify({action: 'deleteContactFromRoom', id}))
+            })
+
+            //delete client
+            rooms.forEach((room )=>{
+                room.clients.find((client, index)=>{
+                    if(client != undefined && client.id === id)
+                    {
+                        room.clients.splice(index, 1);
+                    }
+                })
+            })
+            //remove client
             clients.delete(ws);
+
+            console.log(rooms)
+            console.log(clients);
           });
     })
     /*  start the HAPI service  */
