f5a4d4355e898c32332e756e759eaa7dd5a3dcdf
Mattia <mattia.ceschin@gmail.com>
2022-03-09 14:15:58 +0100

    Add feature notifications
    notifications if you don't already have messagges

--- src/chat-client/src/components/rowChatComponent.vue
+++ src/chat-client/src/components/rowChatComponent.vue
@@ -1,9 +1,17 @@
 <template>
     <template v-if="idContactCurrent === idContact">
-        <a @click="useThisChat" class="list-group-item list-group-item-action m-1 active" id="list-settings-list" data-bs-toggle="list" role="tab" aria-controls="list-settings" style="word-wrap: break-word;">{{nicknameContact}}</a>
+        {{resetNumberNotRead()}}
+        <a @click="useThisChat" class="list-group-item list-group-item-action m-1 active" id="list-settings-list" data-bs-toggle="list" role="tab" aria-controls="list-settings" style="word-wrap: break-word;">
+            {{nicknameContact}}
+        </a>
     </template>
     <template v-else>
-        <a @click="useThisChat" class="list-group-item list-group-item-action m-1" id="list-settings-list" data-bs-toggle="list" role="tab" aria-controls="list-settings" style="word-wrap: break-word;">{{nicknameContact}}</a>
+        <a @click="useThisChat" class="list-group-item list-group-item-action m-1 d-flex justify-content-between align-items-start" id="list-settings-list" data-bs-toggle="list" role="tab" aria-controls="list-settings" style="word-wrap: break-word;">
+            {{nicknameContact}}
+            <template v-if="notread > 0">
+                <span class="badge bg-danger rounded-pill">{{notread}}</span>
+            </template>
+        </a>
     </template>
 </template>
 
@@ -12,11 +20,15 @@
         props:{
             nicknameContact: String,
             idContact:String,
-            idContactCurrent:String
+            idContactCurrent:String,
+            notread:Array
         },
         methods:{
             useThisChat(){
                 this.emitter.emit('change-chat', this.idContact)
+            },
+            resetNumberNotRead(){
+                this.emitter.emit('reset-notread', this.idContact)
             }
         }
     }
